bb8dcbf1eff7d4631c9bb2fb21618c300e90ff20
Андрей Каменский <rybinsk@mail.ru>
2022-08-13 20:22:14 +0300

    add available commands add showHelp method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
+    private static final String HELP_COMMAND = "-help";
+    private static final String INPUT_FILE_COMMAND = "-inputFile";
+    private static final String OUTPUT_FILE_COMMAND = "-outputFile";
+    private static final String DEFAULT_OUTPUT_FILE_NAME = "groupingTable.xls";
+
+
+
     public static void main(String[] args) {
+        ArgumentsProcessor argsProcessor = new ArgumentsProcessor(args);
+        argsProcessor.setAvailableCommands(List.of(HELP_COMMAND, OUTPUT_FILE_COMMAND, INPUT_FILE_COMMAND));
+        argsProcessor.parseArguments();
+        if (argsProcessor.hasCommand(HELP_COMMAND)) {
+            showHelp();
+            return;
+        }
+
+
+
+
         // ParseCommandLine();
         // cl
         // TableLoader loader = new ExcellTableLoader(fileName);
@@ -24,8 +43,7 @@ public class Main {
         //TableSaver saver = new ExcellTableSaver();
 
 
-
-        String templateFile = "src/test/testResources/simpleTable.xlsx";
+        /*String templateFile = "src/test/testResources/simpleTable.xlsx";
 
         FileInputStream inputStream = null;
         try {
@@ -41,7 +59,7 @@ public class Main {
                 wb = new HSSFWorkbook(inputStream);
             }
 
-            for (Sheet sheet : wb ) {
+            for (Sheet sheet : wb) {
                 System.out.printf("Sheet name: %s%n", sheet.getSheetName());
                 for (Row row : sheet) {
                     for (Cell cell : row) {
@@ -50,10 +68,8 @@ public class Main {
                                 cell.getCellType().name());
                         switch (cell.getCellType()) {
                             case STRING -> System.out.printf("String value: \"%s\"%n", cell.getStringCellValue());
-                            case NUMERIC ->
-                                    System.out.printf("Numeric value: %f%n", cell.getNumericCellValue());
-                            case FORMULA ->
-                                    System.out.printf("Formula value: %s%n", cell.getCellFormula());
+                            case NUMERIC -> System.out.printf("Numeric value: %f%n", cell.getNumericCellValue());
+                            case FORMULA -> System.out.printf("Formula value: %s%n", cell.getCellFormula());
                         }
                     }
                 }
@@ -61,7 +77,29 @@ public class Main {
 
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
+    }
+
+    private static void showHelp() {
+        //todo: add description
+        String helpMessage = """
+                description coming soon...
+                Available commands:
+                    %1$s : help.
+                    %2$s : command sets input file name.
+                           No default value. Required command.
+                    %3$s : command sets output file name and format.
+                           Default value is "%4$s"
+                Usage example:
+                java -jar grouptable.jar %2$s table.xlsx %3$s resultTable.xls
+                """;
+        System.out.printf(helpMessage,
+                HELP_COMMAND,
+                INPUT_FILE_COMMAND,
+                OUTPUT_FILE_COMMAND,
+                DEFAULT_OUTPUT_FILE_NAME
+        );
+
     }
 
 }
