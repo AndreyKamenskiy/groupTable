4ae399339bec3ea56e1659401e2476faa7aa7a94
Андрей Каменский <rybinsk@mail.ru>
2022-08-14 22:01:25 +0300

    add some tests

--- src/test/java/org/example/table/TableTest.java
+++ src/test/java/org/example/table/TableTest.java
@@ -30,29 +30,48 @@ class TableTest {
         t.setRow(4, row2);
         assert t.getHeight() == 5;
         assert t.getWidth() == 6;
-        
-
-
-
     }
 
     @Test
     void getRow() {
+        Table t = new Table();
+        assert t.getHeight() == 0;
+        Row r = t.getRow(3);
+        assert t.getHeight() == 4;
+        assert r.getWidth() == 0;
+        assert t.getWidth() == 0;
+        r.setCell(60, new Cell("rrr"));
+        assert t.getWidth() == 61;
+        t.getRow(50);
+        assert t.getHeight() == 51;
+        assert t.getRow(3) == r;
+        assert t.getRow(35).getWidth() == 0;
     }
 
     @Test
     void getCell() {
+        Table t = new Table();
+        Cell c = t.getCell(0, 15);
+        assert c.isEmpty();
+        assert t.getWidth() == 16;
+        assert t.getHeight() == 1;
+        Row r = new Row();
+        r.setCell(55, new Cell(555));
+        t.setRow(1, r);
+        c = t.getCell(1, 55);
+        assert c.getDoubleValue() == 555;
+        assert t.getWidth() == 56;
     }
 
     @Test
     void setCell() {
+        Table t = new Table();
+        t.setCell(50, 100, new Cell("final"));
+        assert t.getWidth() == 101;
+        assert t.getHeight() == 51;
+        t.setCell(50, 100, new Cell());
+        assert t.getWidth() == 0;
+        assert t.getHeight() == 51;
     }
 
-    @Test
-    void getWidth() {
-    }
-
-    @Test
-    void getHeight() {
-    }
 }
