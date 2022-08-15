de5b75e115459ea8af9f2afa9a8a4e69dabdfe57
Андрей Каменский <rybinsk@mail.ru>
2022-08-15 20:18:37 +0300

    unused import deleted, processArgs(args) written, add save excel table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,7 @@
 package org.example;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.table.Table;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,31 +13,25 @@ public class Main {
     private static final String INPUT_FILE_COMMAND = "-inputFile";
     private static final String OUTPUT_FILE_COMMAND = "-outputFile";
     private static final String DEFAULT_OUTPUT_FILE_NAME = "groupingTable.xls";
+    private static final int UNKNOWN_FORMAT_EXIT_STATUS = 1;
+    private static final int LOAD_ERROR_EXIT_STATUS = 2;
+    private static final int SAVE_ERROR_EXIT_STATUS = 3;
+    private static final int OK_EXIT_STATUS = 0;
 
-
+    private static String inputFileName;
+    private static String outputFileName = DEFAULT_OUTPUT_FILE_NAME;
 
     public static void main(String[] args) {
-        ArgumentsProcessor argsProcessor = new ArgumentsProcessor(args);
-        argsProcessor.setAvailableCommands(List.of(HELP_COMMAND, OUTPUT_FILE_COMMAND, INPUT_FILE_COMMAND));
-        argsProcessor.parseArguments();
-        if (argsProcessor.hasCommand(HELP_COMMAND)) {
-            showHelp();
-            return;
-        }
-        if (!argsProcessor.hasCommandWithValue(INPUT_FILE_COMMAND)) {
-            System.out.printf("Input file name is absent. Use: %s <filename>", INPUT_FILE_COMMAND);
-            return;
-        }
-        String fileName = argsProcessor.getValue(INPUT_FILE_COMMAND);
+        processArgs(args);
 
-        TableLoader loader = chooseLoader(fileName);
+        TableLoader loader = createTableLoader(inputFileName);
 
-        Table table;
+        Table table = null;
         try {
-            table = loader.loadTable(fileName);
+            table = loader.loadTable(inputFileName);
         } catch (IOException e) {
-            System.out.printf("Load table error: %s", e.getMessage());
-            return;
+            System.out.printf("Table load error: %s%n", e.getMessage());
+            System.exit(LOAD_ERROR_EXIT_STATUS);
         }
 
         for (int row = 0; row < table.getHeight(); ++row) {
@@ -56,13 +41,48 @@ public class Main {
             System.out.print('\n');
         }
 
-
+        TableSaver saver = createTableSaver(outputFileName);
+        try {
+            saver.saveTable(outputFileName, table);
+        } catch (IOException e) {
+            System.out.printf("Table save error: %s%n", e.getMessage());
+            System.exit(SAVE_ERROR_EXIT_STATUS);
+        }
     }
 
-    private static TableLoader chooseLoader(String fileName) {
+    private static void processArgs(String[] args) {
+        ArgumentsProcessor argsProcessor = new ArgumentsProcessor(args);
+        argsProcessor.setAvailableCommands(List.of(HELP_COMMAND, OUTPUT_FILE_COMMAND, INPUT_FILE_COMMAND));
+        argsProcessor.parseArguments();
+        if (argsProcessor.hasCommand(HELP_COMMAND)) {
+            showHelp();
+            System.exit(OK_EXIT_STATUS);
+        }
+        if (!argsProcessor.hasCommandWithValue(INPUT_FILE_COMMAND)) {
+            System.out.printf("Input file name is absent. Use: %s <filename>%n", INPUT_FILE_COMMAND);
+            System.exit(LOAD_ERROR_EXIT_STATUS);
+        }
+        inputFileName = argsProcessor.getValue(INPUT_FILE_COMMAND);
+        if (argsProcessor.hasCommandWithValue(INPUT_FILE_COMMAND)) {
+            outputFileName = argsProcessor.getValue(OUTPUT_FILE_COMMAND);
+        }
+    }
+
+    private static TableLoader createTableLoader(String fileName) {
         if (fileName.matches(".+[.]xlsx?")) {
             return new ExcelTableLoader();
         }
+        System.out.printf("Cannot read file %s. Unknown format", fileName);
+        System.exit(UNKNOWN_FORMAT_EXIT_STATUS);
+        return null;
+    }
+
+    private static TableSaver createTableSaver(String fileName) {
+        if (fileName.matches(".+[.]xlsx?")) {
+            return new ExcelTableSaver();
+        }
+        System.out.printf("Cannot write file %s. Unknown format", fileName);
+        System.exit(UNKNOWN_FORMAT_EXIT_STATUS);
         return null;
     }
 
@@ -77,7 +97,7 @@ public class Main {
                     %3$s : command sets output file name and format.
                            Default value is "%4$s"
                 Usage example:
-                java -jar grouptable.jar %2$s table.xlsx %3$s resultTable.xls
+                java -jar groupTable.jar %2$s table.xlsx %3$s resultTable.xls
                 """;
         System.out.printf(helpMessage,
                 HELP_COMMAND,
